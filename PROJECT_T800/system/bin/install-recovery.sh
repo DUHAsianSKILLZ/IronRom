0f469158c6209e75f8e0c9f923862d847c26cf5d
Tkkg1994 <luca.grifo@outlook.com>
2015-04-28 23:58:17 +0200

    Update T800 to new build T800ZSU1BOD1, first commit after HDD crash

--- PROJECT_T800/system/bin/install-recovery.sh
+++ PROJECT_T800/system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY:7647232:fd41b14184e3ae011cb0e0cebe4126992a8e6ce1; then
-  applypatch EMMC:/dev/block/platform/dw_mmc.0/by-name/BOOT:7434240:1c3544212073594a48bcfcffe0695cf5b25783e6 EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY fd41b14184e3ae011cb0e0cebe4126992a8e6ce1 7647232 1c3544212073594a48bcfcffe0695cf5b25783e6:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY:7647232:fa7c74bff7fc3c771271455b2acdc9d0950a9635; then
+  applypatch EMMC:/dev/block/platform/dw_mmc.0/by-name/BOOT:7434240:2255815dc1c8b35725bc3b4fc85bdb6027c977c8 EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY fa7c74bff7fc3c771271455b2acdc9d0950a9635 7647232 2255815dc1c8b35725bc3b4fc85bdb6027c977c8:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
